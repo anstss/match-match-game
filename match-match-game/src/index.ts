db18254c651e5f9584c0b89915b42115a8caa47c
anstss <anastasiya31017@gmail.com>
2021-05-10 22:16:25 +0300

    feat: configured webpack, eslint, prettier

---
+++ match-match-game/src/index.ts
@@ -0,0 +1,13 @@
+import './styles.scss';
+
+document.body.innerHTML = `
+  <p>Hello</p>
+`;
+
+// console.log('TEST');
+
+// function sum(a: number, b: number) {
+//   return a + b;
+// }
+// console.log(sum(5, 6));
+// sum(5, 6);
---
+++ match-match-game/webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+// const PrettierPlugin = require('prettier-webpack-plugin');
+
+const devServer = (isDevMode) => !isDevMode ? {} : {
+  devServer: {
+    open: true,
+    port: 8080,
+    contentBase: path.join(__dirname, 'public'),
+  }
+};
+
+const esLintPlugin = (isDevMode) => isDevMode ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
+
+
+module.exports = ({devMode}) => ({
+  mode: devMode ? 'development' : 'production',
+  devtool: devMode ? 'inline-source-map' : false,
+  entry: {
+    app: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // title: 'Match-Match Game',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './public' },
+      ],
+    }),
+    // new PrettierPlugin(),
+    ...esLintPlugin(devMode),
+  ],
+  ...devServer(devMode),
+});

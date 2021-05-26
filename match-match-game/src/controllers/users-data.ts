427db37459b90b59978f47a7cc6c5fa4b115a87d
anstss <anastasiya31017@gmail.com>
2021-05-27 00:42:38 +0300

    fix: getTopTen only from game-users keys

--- match-match-game/src/controllers/users-data.ts
+++ match-match-game/src/controllers/users-data.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash';
 import { UsersDataInterface } from '../models/users-data-interface';
-import { MAX_SIZE_IMG_BYTES } from '../shared/constans';
+import { MATCH_USER_ID, MAX_SIZE_IMG_BYTES } from '../shared/constans';
 import { ModalRegister } from '../shared/modals/modal-register';
 import { GameController } from './game-controller';
 
@@ -50,15 +50,15 @@ export class UsersData {
 
   addUser() {
     const user = JSON.stringify(this.getUserInfo());
-    const userID = Date.now();
-    window.localStorage.setItem(userID.toString(), user);
+    const userID = MATCH_USER_ID + Date.now();
+    window.localStorage.setItem(userID, user);
   }
 
   static clearStorage() {
     const topTen = UsersData.getTopTen();
     window.localStorage.clear();
     topTen.forEach((player) => {
-      localStorage.setItem(Date.now().toString(), JSON.stringify(player));
+      localStorage.setItem(MATCH_USER_ID + Date.now(), JSON.stringify(player));
     });
   }
 
@@ -71,7 +71,9 @@ export class UsersData {
     // special for eslint "no-restricted-syntax"
     for (let i = 0; i < window.localStorage.length; i += 1) {
       const key = window.localStorage.key(i);
-      allUsers.push(JSON.parse(window.localStorage.getItem(key!)!));
+      if (key?.includes(MATCH_USER_ID)) {
+        allUsers.push(JSON.parse(window.localStorage.getItem(key)!));
+      }
     }
     const topTen = _.sortBy(allUsers, 'score').reverse().slice(0, 10);
     return topTen;
--- match-match-game/src/shared/constans.ts
+++ match-match-game/src/shared/constans.ts
@@ -9,6 +9,8 @@ export const TIME_DELAY_BEFORE_SHOW_CORRECTNESS = 300;
 
 export const MAX_SIZE_IMG_BYTES = 104858;
 
+export const MATCH_USER_ID = 'matchMatchUser';
+
 export const REGEXP_SPECIAL_CHARACTERS = /[~!@#$%*()_—+=|:;"'`<>,.?/^]/g;
 // eslint-disable-next-line
 export const REGEXP_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
